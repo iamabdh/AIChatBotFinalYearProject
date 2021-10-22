367cf4354e5ad48ed38b66e4e4d92bc5fc3185fd
iamabdh <abdullahalbadri09@gmail.com>
2021-10-22 17:20:34 +0400

    edit collages scraping

--- webScraping/scrapingColleges.py
+++ webScraping/scrapingColleges.py
@@ -6,7 +6,15 @@ page = urlopen(url + 'agriculture/About-College')
 html = page.read().decode('utf-8')
 aboutCAMS_html = BeautifulSoup(html, "html.parser")
 
-aboutParagragh_html = aboutCAMS_html.find_all('div', {'class': 'resp_margin'}) 
+CAMSParagragh_html = aboutCAMS_html.find_all('div', {'id': 'tab_item-7237-1'})[0].find_all('p') 
+
+CAMS_text = str()
+
+for textItem in CAMSParagragh_html:
+    CAMS_text += textItem.text +'\n'
+
+print(CAMS_text)
 
 
-print(aboutParagragh_html[0].find_all('p'))
+
+
