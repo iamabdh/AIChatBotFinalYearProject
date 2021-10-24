5221c99d5974090c7c82597622fb6efd0cc5f8a0
Ahmed96Ali <s120181@student.squ.edu.om>
2021-10-24 19:06:01 +0400

    new Changes

--- webScraping/scrapingColleges.py
+++ webScraping/scrapingColleges.py
@@ -16,5 +16,32 @@ for textItem in CAMSParagragh_html:
 print(CAMS_text)
 
 
+page1 = urlopen(url + 'agriculture/Academic-Department/Animal-and-Veterinary-Sciences/Description') 
+html1 = page1.read().decode('utf-8')
+aboutAVS_descr_html = BeautifulSoup(html1, "html.parser")
+
+AVS_descrParagragh_html = aboutAVS_descr_html.find_all('div', {'id': 'tab_item-1723-0'})[0].find_all('p') 
+
+AVSdescr_text = str()
+
+for textItem in AVS_descrParagragh_html:
+    AVSdescr_text += textItem.text +'\n'
+
+print(AVSdescr_text)
+
+page2 = urlopen(url + 'agriculture/Academic-Department/Plant-Sciences/Description') 
+html2 = page2.read().decode('utf-8')
+aboutPS_descr_html = BeautifulSoup(html2, "html.parser")
+
+PS_descrParagragh_html = aboutPS_descr_html.find_all('div', {'id': 'tab_item-1724-0'})[0].find_all('p') 
+
+PSdescr_text = str()
+
+for textItem in PS_descrParagragh_html:
+    PSdescr_text += textItem.text +'\n'
+
+print(PSdescr_text)
+
+
 
 
