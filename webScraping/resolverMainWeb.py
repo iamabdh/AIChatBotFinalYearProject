3a36a4b0aa945c148ab0b67d5d6f37b7f233141b
Ahmed96Ali <s120181@student.squ.edu.om>
2021-11-15 13:16:41 +0400

    added intents

--- webScraping/resolverMainWeb.py
+++ webScraping/resolverMainWeb.py
@@ -18,11 +18,21 @@ class ResolverMainPage:
             listingName.remove(listingName[0])
             name = ' '.join(listingName)
          return facultyStaff.searchFF(name)
+    
+    def engineer():
+        from webScraping.scrapingENG2 import Engineer
+        return Engineer.electrical()
+
+
+    
 
 
 
 def resloverIntents(int, arg = None):
     return {
         'AboutSQU' : ResolverMainPage.AboutPage(),
+        'Electrical': ResolverMainPage.engineer(),
         'searchFF' : ResolverMainPage.FFSearch(arg),
     }.get(int, 0) # return 0 if the resolver doesnt find match requests
+
+print(resloverIntents('Electrical'))
