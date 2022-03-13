10dbde1cb84a6425b537072487ceac8dd1823cc3
Ahmed96Ali <s120181@student.squ.edu.om>
2022-03-14 01:11:06 +0400

    searchCourse and fix bugs

--- webScraping/courseSearch.py
+++ webScraping/courseSearch.py
@@ -26,19 +26,26 @@ def searchCourse(course):
         rquiredObj.append(objItem)
 
     courseCode = course
-    courseList = []
+    info=str()
     if courseCode is not None:
         for index, courseObj in enumerate(rquiredObj):
-            courseObj=courseObj[0].split()
-            
-            for l in courseObj:
-                if courseCode.upper() == l:
-                    info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
-                    
-                    courseList.append(info)
-                    break
+            if courseObj[0] != '':
+                courseObj=courseObj[0].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
+
+                        return info
+            else:
+                courseObj=courseObj[1].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
+                        info=info[1:]
+
+                        return info
     
-    return {'Course Info: ': courseList}
+    
 
 def searchCourseArc(course):
     
@@ -56,21 +63,20 @@ def searchCourseArc(course):
         rquiredObj.append(objItem)
 
     courseCode = course
-    courseList = []
+    info=str()
     if courseCode is not None:
         for index, courseObj in enumerate(rquiredObj):
             courseObj=courseObj[0].split()
             
             for l in courseObj:
                 if courseCode.upper() == l:
-                    info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
-                    
-                    courseList.append(info)
-                    break
+                    info=rquiredObj[index]+rquiredObj[index+1]
+
+                    return info
 def searchCourseCIVL(course):
     
     url = 'https://www.squ.edu.om/engineering/Academic/Undergraduate-Programs/-Civil-Engineering'
-
+    course= course[4:]
     coursePage = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'lxml').find_all(['p'])
     objectList = []
     for itemTotal in coursePage:
@@ -83,27 +89,171 @@ def searchCourseCIVL(course):
         rquiredObj.append(objItem)
 
     courseCode = course
-    courseList = []
+    info=str()
     if courseCode is not None:
         for index, courseObj in enumerate(rquiredObj):
             courseObj=courseObj[0].split()
             
             for l in courseObj:
                 if courseCode.upper() == l:
-                    info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
+                    info=rquiredObj[index]+rquiredObj[index+1]
+
+                    return info
+
+def searchCourseIndust(course):
+    
+    url = 'https://www.squ.edu.om/engineering/Academic/Undergraduate-Programs/Industrial-Engineering'
+    url2 = 'https://www.squ.edu.om/engineering/Academic/Undergraduate-Programs/Mechanical-Engineering'
+
+
+    coursePage = BeautifulSoup(urlopen(url).read().decode('utf-8'),'html.parser').find_all('div', {'id': 'ResponsiveTabs_6032'})[0].find_all(['p'])
+    coursePage2 = BeautifulSoup(urlopen(url2).read().decode('utf-8'),'html.parser').find_all('div', {'id': 'ResponsiveTabs_6033'})[0].find_all(['p'])
+    objectList = []
+    for itemTotal in coursePage2:
+        objectList.append(itemTotal.text.splitlines())
+    for itemTotal in coursePage:
+        objectList.append(itemTotal.text.splitlines())
+    
+
+    rquiredObj = []
+
+    for objItem in objectList:
+        rquiredObj.append(objItem)
+    
+    courseCode = course
+    info=str()
+    if courseCode is not None:
+        for index, courseObj in enumerate(rquiredObj):
+            if courseObj[0] != '':
+                courseObj=courseObj[0].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
+                        if len(info[3])<5:
+                            info=info[:3]
+
+                        return info
                     
-                    courseList.append(info)
-                    break        
+            elif courseObj[0] == '':
+                courseObj=courseObj[1].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]
+                        info=info[1:]
+
+                        return info
+
+def searchCourseMechat(course):
+
+    url = 'https://www.squ.edu.om/engineering/Academic/Undergraduate-Programs/Mechatronics-Engineering'
+
+    coursePage = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'html.parser').find_all('div', {'id': 'ResponsiveTabs_6034'})[0].find_all(['p'])
+    objectList = []
+    for itemTotal in coursePage:
+        objectList.append(itemTotal.text.splitlines())
 
 
-    return courseList
+    rquiredObj = []
 
-y=input("Enter course code: ")
+    for objItem in objectList:
+        rquiredObj.append(objItem)
 
-if (y[:4]=="ECCE")or(y[:4]=="ecce"):
-    print(searchCourse(y))
-# elif (y[:4]=="AREN")or(y[:4]=="aren"):
-#     print(searchCourseArc(y))
-# elif (y[:4]=="CIVL")or(y[:4]=="civl"):
-#     print(searchCourseCIVL(y))
+    courseCode = course
+    info=str()
+    if courseCode is not None:
+        for index, courseObj in enumerate(rquiredObj):
+            if courseObj[0] != '':
+                courseObj=courseObj[0].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]+rquiredObj[index+3]
+                        if len(info[3])<5:
+                            info=info[:3]
 
+                        return info
+            else:
+                courseObj=courseObj[1].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]+rquiredObj[index+3]
+                        info=info[1:]
+
+                        return info
+
+def searchCourseChem(course):
+
+    url = 'https://www.squ.edu.om/engineering/Academic/Undergraduate-Programs/Chemical-and-Process-Engineering'
+
+    coursePage = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'html.parser').find_all('div', {'id': 'ResponsiveTabs_6035'})[0].find_all(['p'])
+    objectList = []
+    for itemTotal in coursePage:
+        objectList.append(itemTotal.text.splitlines())
+
+
+    rquiredObj = []
+
+    for objItem in objectList:
+        rquiredObj.append(objItem)
+
+    courseCode = course
+    info=str()
+    if courseCode is not None:
+        for index, courseObj in enumerate(rquiredObj):
+            if courseObj[0] != '':
+                courseObj=courseObj[0].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]+rquiredObj[index+3]
+                        if len(info[3])<5:
+                            info=info[:3]
+
+                        return info
+            else:
+                courseObj=courseObj[1].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]+rquiredObj[index+3]
+                        info=info[1:]
+
+                        return info
+
+def searchCoursePetro(course):
+
+    url = 'https://www.squ.edu.om/engineering/Academic/Undergraduate-Programs/Petroleum-and-Natural-Gas-Engineering'
+
+    coursePage = BeautifulSoup(urlopen(url).read().decode('utf-8'), 'html.parser').find_all('div', {'id': 'ResponsiveTabs_6036'})[0].find_all(['p'])
+    objectList = []
+    for itemTotal in coursePage:
+        objectList.append(itemTotal.text.splitlines())
+
+
+    rquiredObj = []
+
+    for objItem in objectList:
+        rquiredObj.append(objItem)
+
+    courseCode = course
+    info=str()
+    if courseCode is not None:
+        for index, courseObj in enumerate(rquiredObj):
+            if courseObj[0] != '':
+                courseObj=courseObj[0].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]+rquiredObj[index+3]
+                        if len(info[3])<5:
+                            info=info[:3]
+
+                        return info
+            else:
+                courseObj=courseObj[1].split()
+                for l in courseObj:
+                    if courseCode.upper() == l:
+                        info=rquiredObj[index]+rquiredObj[index+1]+rquiredObj[index+2]+rquiredObj[index+3]
+                        info=info[1:]
+
+                        return info
+    
+
+
+# print(searchCoursePetro("pnge4512"))
--- webScraping/resolverMainWeb.py
+++ webScraping/resolverMainWeb.py
@@ -25,6 +25,24 @@ class ResolverMainPage:
             listYeareDegree=yearDegree.split()
             year=listYeareDegree[0]
         return degreePlanF.degreePlan(year)
+    def courseSearch(name):
+         from webScraping import courseSearch
+         listingName = name.split()
+         listname=listingName[0]+listingName[1]
+         if (listname[:4]=="ECCE")or(listname[:4]=="ecce"):
+            return courseSearch.searchCourse(listname)
+         elif (listname[:4]=="AREN")or(listname[:4]=="aren"):
+            return courseSearch.searchCourseArc(listname)
+         elif (listname[:4]=="CIVL")or(listname[:4]=="civl"):
+            return courseSearch.searchCourseCIVL(listname)
+         elif (listname[:4]=="MEIE")or(listname[:4]=="meie"):
+            return courseSearch.searchCourseIndust(listname)
+         elif (listname[:4]=="MCTE")or(listname[:4]=="mcte"):
+            return courseSearch.searchCourseMechat(listname)
+         elif (listname[:4]=="CHPE")or(listname[:4]=="chpe"):
+            return courseSearch.searchCourseChem(listname)
+         elif (listname[:4]=="PNGE")or(listname[:4]=="pnge"):
+            return courseSearch.searchCoursePetro(listname)
     
     def JobServices():
         from webScraping.Services import Services
